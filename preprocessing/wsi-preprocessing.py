54e6189f2ded26885b953575524564db4fac4cf7
Felipe <fcoelho.mrc@gmail.com>
2024-11-11 19:12:03 +0000

    Tiling...

--- preprocessing/wsi-preprocessing.py
+++ preprocessing/wsi-preprocessing.py
@@ -5,16 +5,22 @@ import cv2
 from skimage.segmentation import clear_border
 
 from openslide import open_slide
+from openslide.deepzoom import DeepZoomGenerator
 from PIL import Image
 
 
-class Slide:
+class SlideManager:
     EXT = "svs"
     THUMB_EXT = "png"
     def __init__(self, slide_dir, thumbnail_dir, mask_dir):
         self.slide_dir = slide_dir
         self.thumbnail_dir = thumbnail_dir
         self.mask_dir = mask_dir
+
+    def get_slide(self, slide):
+        slide_path = os.path.join(slide_dir, self.slide_dir, f"{slide}.{self.EXT}")
+        wsi = open_slide(slide_path)
+        return wsi
 
     def get_thumbnail(self, slide):
         thumbnail_path = os.path.join(self.thumbnail_dir, f"{slide}_thumb.{self.THUMB_EXT}")
@@ -26,8 +32,44 @@ class Slide:
         mask = Image.open(mask_path)
         return mask
 
+    def get_tiles(
+            self,
+            slide,
+            tile_size=256,
+            overlap=0,
+            limit_bounds=True,
+            max_background_percent= 0,
+            level=0  # highest resolution
+    ):
+        wsi = self.get_slide(slide)
+        mask = np.array(self.get_mask(slide))  # assume mask is pre-generated and stored
+        dz = DeepZoomGenerator(
+            wsi, tile_size=tile_size, overlap=overlap, limit_bounds=limit_bounds
+        )
+        tile_grid_width, tile_grid_height = dz.level_tiles[-1]
+        dz_level = len(dz.level_dimensions) - 1
 
-class SlideProcessor:
+        # Assumptions!
+        ## thumbnail shape = slide largest downsample shape
+        ## slide properties -> patch size -> tiling matches thumbnail shape!
+        delta = tile_size / wsi.level_downsamples[-1]
+        tiles = []
+        tiles_coordinates = []
+        from tqdm import tqdm
+        pbar = tqdm(total=tile_grid_height*tile_grid_width)
+        for i in range(tile_grid_width):
+            for j in range(tile_grid_height):
+                tile = dz.get_tile(level=dz_level, address=(i, j))
+                tile = np.array(tile)
+                if mask[int(j * delta), int(i * delta)] > 0:
+                    tiles.append(tile)  # TODO: discard edge tiles with too much background
+                    tiles_coordinates.append((i, j))
+
+                pbar.update()
+        return tiles, tiles_coordinates
+
+
+class MaskEngine:
 
     def __init__(self, thumbnail):
         self.thumbnail = np.array(thumbnail, dtype=np.uint8)
@@ -43,8 +85,9 @@ class SlideProcessor:
 
         # pipeline
         self.mask = None
+        self.regions = None
         self.thresholding()
-        self.mask2 = self.select_region()
+        self.select_region()
 
 
     def thresholding(self):
@@ -62,7 +105,7 @@ class SlideProcessor:
 
         # Second pass - keep only biggest connected object
         regions = self._select_region(regions, threshold=self.selection_threshold)  # only keep largest region
-        return regions
+        self.mask = regions * self.mask
 
     def _select_region(self, target, threshold):
         contours, hierarchy = cv2.findContours(target, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -80,6 +123,10 @@ class SlideProcessor:
             cv2.fillPoly(regions, pts=box, color=255)
         return regions
 
+    def get_mask(self):
+        return self.mask
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
@@ -94,33 +141,54 @@ if __name__ == "__main__":
     thumbnail_dir = os.path.join(root_dir, "TCGA_CRC_thumbs", "thumbnails")
     mask_dir = os.path.join(root_dir, "TCGA_CRC_thumbs", "mask")
 
+    test_preprocessing = False
 
-    for sample in samples:
-        slide = Slide(slide_dir, thumbnail_dir, mask_dir)
+    slide = SlideManager(slide_dir, thumbnail_dir, mask_dir)
+    wsi = slide.get_slide(samples[0])
+    # inspect wsi
+    print(f"level_count", wsi.level_count)
+    print(f"dimensions", wsi.dimensions)
+    print(f"level_dimensions", wsi.level_dimensions)
+    print(f"level_downsamples", wsi.level_downsamples)
+    print(f"properties", wsi.properties)
+    print(f"color_profile", wsi.color_profile)
+    print(f"associated_images", wsi.associated_images)
 
-        mask = slide.get_mask(sample)
-        mask_arr = np.array(mask)
+    tiles, tiles_coordinates = slide.get_tiles(samples[0])
 
-        thumbnail = slide.get_thumbnail(sample)
-        thumbnail_arr = np.array(thumbnail)
+    print(f"number of tiles", len(tiles))
+    count = 0
+    maxtiles = 5
+    fig, axs = plt.subplots(1, maxtiles)
+    while count <= maxtiles:
+        axs[count].imshow(tiles[count])
+        count += 1
+    fig.suptitle("my first TCGA self-made tiles :)")
+    plt.show()
 
-        proc = SlideProcessor(thumbnail)
+    if test_preprocessing:
+        for sample in samples:
+            slide = SlideManager(slide_dir, thumbnail_dir, mask_dir)
 
-        test = proc.mask.copy()
-        test2 = proc.mask2.copy()
+            mask = slide.get_mask(sample)
+            mask_arr = np.array(mask)
 
+            thumbnail = slide.get_thumbnail(sample)
+            thumbnail_arr = np.array(thumbnail)
 
-        plt.imshow(thumbnail_arr)
-        plt.show()
+            proc = MaskEngine(thumbnail)
 
-        fig, axs = plt.subplots(1, 4)
-        axs[0].imshow(mask_arr, cmap="gray")
-        axs[1].imshow(test, cmap="gray")
-        axs[2].imshow(test2, cmap="gray")
-        axs[3].imshow(test * test2, cmap="gray")
+            test = proc.get_mask()
 
-        fig.tight_layout()
-        plt.show()
+            plt.imshow(thumbnail_arr)
+            plt.show()
+
+            fig, axs = plt.subplots(1, 2)
+            axs[0].imshow(mask_arr, cmap="gray")
+            axs[1].imshow(test, cmap="gray")
+
+            fig.tight_layout()
+            plt.show()
 
 
 
